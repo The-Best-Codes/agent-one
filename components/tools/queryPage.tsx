a64388c746c13227d2fed362372e07197b849147
The-Best-Codes <bestcodes.official@gmail.com>
2025-02-27 15:49:22 -0600

    attempt to fix query and regex page styles

--- components/tools/queryPage.tsx
+++ components/tools/queryPage.tsx
@@ -26,85 +26,78 @@ export const QueryPage: React.FC<QueryProps> = ({
   return (
     <div className="border rounded-xl p-0 px-2 my-4 motion-preset-blur-right">
       <div className="flex w-full">
-        {isLoading
-          ? (
-            <div className="py-2 flex flex-row space-x-2 w-full">
-              <Loader className="w-6 h-6 min-w-6 min-h-6" />
-              <p className="text-base font-medium max-w-full overflow-auto whitespace-nowrap">
-                Finding "
-                <span className="font-mono bg-gray-100 px-1">{selector}</span>"
-                elements on{" "}
-                <Link
-                  href={url}
-                  target="_blank"
-                  className="text-blue-500 cursor-pointer"
-                >
-                  {url}
-                </Link>
-              </p>
-            </div>
-          )
-          : results
-          ? (
-            <Accordion
-              className="w-full"
-              type="single"
-              collapsible
-              defaultValue={undefined}
-            >
-              <AccordionItem
-                className="border-none w-full"
-                value="query-results"
+        {isLoading ? (
+          <div className="py-2 flex flex-row space-x-2 w-full">
+            <Loader className="w-6 h-6 min-w-6 min-h-6" />
+            <p className="text-base font-medium max-w-full overflow-auto whitespace-nowrap">
+              Finding "
+              <span className="font-mono bg-gray-100 px-1">{selector}</span>"
+              elements on{" "}
+              <Link
+                href={url}
+                target="_blank"
+                className="text-blue-500 cursor-pointer"
               >
-                <AccordionTrigger className="text-base w-full py-2 font-medium">
-                  <div className="flex flex-row max-w-full">
-                    <Code className="w-6 h-6 min-w-6 min-h-6 mr-2" />
-                    <p className="text-base font-medium max-w-96 truncate">
-                      Searched for "
-                      <span className="font-mono bg-gray-100 px-1">
-                        {selector}
-                      </span>
-                      " elements on{" "}
-                      <Link
-                        href={url}
-                        target="_blank"
-                        className="text-blue-500 cursor-pointer"
-                      >
-                        {url}
-                      </Link>
-                    </p>
-                  </div>
-                </AccordionTrigger>
-                <AccordionContent className="overflow-auto max-h-64">
-                  <div className="rounded-xl mt-2 p-0 px-2">
-                    <pre>
+                {url}
+              </Link>
+            </p>
+          </div>
+        ) : results ? (
+          <Accordion
+            className="w-full"
+            type="single"
+            collapsible
+            defaultValue={undefined}
+          >
+            <AccordionItem className="border-none w-full" value="query-results">
+              <AccordionTrigger className="text-base w-full py-2 font-medium">
+                <div className="flex flex-row max-w-full">
+                  <Code className="w-6 h-6 min-w-6 min-h-6 mr-2" />
+                  <p className="text-base font-medium max-w-96 truncate">
+                    Searched for "
+                    <span className="font-mono bg-gray-100 dark:bg-gray-800 px-1">
+                      {selector}
+                    </span>
+                    " elements on{" "}
+                    <Link
+                      href={url}
+                      target="_blank"
+                      className="text-blue-500 cursor-pointer"
+                    >
+                      {url}
+                    </Link>
+                  </p>
+                </div>
+              </AccordionTrigger>
+              <AccordionContent className="overflow-auto max-h-64">
+                <div className="rounded-xl mt-2 p-0 px-2">
+                  <pre>
                     <p className="text-sm text-gray-600 font-mono">
                       {results.result}
                     </p>
-                    </pre>
-                  </div>
-                </AccordionContent>
-              </AccordionItem>
-            </Accordion>
-          )
-          : (
-            <>
-              <Code className="w-6 h-6 min-w-6 min-h-6 text-gray-500" />
-              <p className="text-base font-medium max-w-full overflow-auto whitespace-nowrap">
-                Searched for "
-                <span className="font-mono bg-gray-100 px-1">{selector}</span>"
-                elements on{" "}
-                <Link
-                  href={url}
-                  target="_blank"
-                  className="text-blue-500 cursor-pointer"
-                >
-                  {url}
-                </Link>
-              </p>
-              <p className="text-sm text-gray-500 mt-2">No results found.</p>
-            </>
-          )}
+                  </pre>
+                </div>
+              </AccordionContent>
+            </AccordionItem>
+          </Accordion>
+        ) : (
+          <>
+            <Code className="w-6 h-6 min-w-6 min-h-6 text-gray-500" />
+            <p className="text-base font-medium max-w-full overflow-auto whitespace-nowrap">
+              Searched for "
+              <span className="font-mono bg-gray-100 px-1">{selector}</span>"
+              elements on{" "}
+              <Link
+                href={url}
+                target="_blank"
+                className="text-blue-500 cursor-pointer"
+              >
+                {url}
+              </Link>
+            </p>
+            <p className="text-sm text-gray-500 mt-2">No results found.</p>
+          </>
+        )}
       </div>
     </div>
   );
