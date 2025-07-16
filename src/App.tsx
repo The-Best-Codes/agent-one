ac6d0765f387a51f581ce1e35dcdfc34986fd048
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-16 14:12:02 -0500

    client-side only google + ai sdk test

--- src/App.tsx
+++ src/App.tsx
@@ -1,29 +1,60 @@
-import { invoke } from "@tauri-apps/api/core";
+import { createGoogleGenerativeAI } from "@ai-sdk/google";
+import { useChat } from "@ai-sdk/react";
+import { convertToModelMessages, DefaultChatTransport, streamText } from "ai";
 import { useState } from "react";
 
-function App() {
-  const [greetMsg, setGreetMsg] = useState("");
-  const [name, setName] = useState("");
+const google = createGoogleGenerativeAI({
+  apiKey: import.meta.env.VITE_GOOGLE_GENERATIVE_AI_API_KEY,
+});
 
-  async function greet() {
-    setGreetMsg(await invoke("greet", { name }));
-  }
+const customFetch = async (_input: RequestInfo | URL, options: any) => {
+  const m = JSON.parse(options.body) as any;
+  console.log(m);
+  const result = streamText({
+    model: google("gemini-2.0-flash"),
+    messages: convertToModelMessages(m.messages),
+    abortSignal: options.signal,
+  });
+  return result.toUIMessageStreamResponse();
+};
+
+function App() {
+  const [input, setInput] = useState("");
+
+  const { error, messages, sendMessage } = useChat({
+    transport: new DefaultChatTransport({
+      fetch: customFetch,
+    }),
+  });
 
   return (
     <main>
+      {messages.map((message) => (
+        <div key={message.id}>
+          {message.role === "user" ? "User: " : "AI: "}
+          {message.parts.map((part, i) => {
+            switch (part.type) {
+              case "text":
+                return <div key={`${message.id}-${i}`}>{part.text}</div>;
+            }
+          })}
+        </div>
+      ))}
+
       <form
         onSubmit={(e) => {
           e.preventDefault();
-          greet();
+          sendMessage({ text: input });
+          setInput("");
         }}
       >
+        {error && <div>Error: {error.message}</div>}
         <input
-          onChange={(e) => setName(e.currentTarget.value)}
-          placeholder="Enter a name..."
+          value={input}
+          placeholder="Say something..."
+          onChange={(e) => setInput(e.currentTarget.value)}
         />
-        <button type="submit">Greet</button>
       </form>
-      <p>{greetMsg}</p>
     </main>
   );
 }
