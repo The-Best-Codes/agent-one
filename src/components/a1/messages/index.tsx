1c78894840f37cc671a4aebc26fce1da32cb0301
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-17 09:58:23 -0500

    testing memoized markdown

--- src/components/a1/messages/index.tsx
+++ src/components/a1/messages/index.tsx
@@ -1,26 +1,32 @@
 import type { UIMessage } from "ai";
 import { MessagePartsGroup } from "./parts-group";
-
 import { MessagePartFallback } from "./parts/fallback";
 import { MessagePartStepStart } from "./parts/step-start";
 import { MessagePartText } from "./parts/text";
-
-const partsMap = {
-  text: MessagePartText,
-  "step-start": MessagePartStepStart,
-};
 
 export const MessageParts = ({ message }: { message: UIMessage }) => {
   return (
     <MessagePartsGroup>
       {message.parts.map((part: (typeof message.parts)[number], i) => {
-        const Part = (partsMap as any)[part.type];
-        if (!Part) {
-          console.error(`Unknown or unhandled message part type: ${part.type}`);
-          return <MessagePartFallback key={`${message.id}-${i}`} {...part} />;
+        switch (part.type) {
+          case "text":
+            return (
+              <MessagePartText
+                key={`${message.id}-${i}`}
+                id={message.id}
+                {...part}
+              />
+            );
+          case "step-start":
+            return (
+              <MessagePartStepStart key={`${message.id}-${i}`} {...part} />
+            );
+          default:
+            console.error(
+              `Unknown or unhandled message part type: ${part.type}`,
+            );
+            return <MessagePartFallback key={`${message.id}-${i}`} {...part} />;
         }
-
-        return <Part key={`${message.id}-${i}`} {...part} />;
       })}
     </MessagePartsGroup>
   );
--- src/components/a1/messages/parts/text.tsx
+++ src/components/a1/messages/parts/text.tsx
@@ -1,7 +1,12 @@
+import { MemoizedMarkdown } from "@/components/a1/markdown/memoized-markdown";
 import type { TextUIPart } from "ai";
 
-export const MessagePartText = ({ ...props }: TextUIPart) => {
+type MessagePartTextProps = TextUIPart & { id: string };
+
+export const MessagePartText = ({ id, text }: MessagePartTextProps) => {
   return (
-    <div className="max-w-full overflow-auto rounded-md">{props.text}</div>
+    <div className="max-w-full overflow-auto rounded-md">
+      <MemoizedMarkdown id={id} content={text} />
+    </div>
   );
 };
