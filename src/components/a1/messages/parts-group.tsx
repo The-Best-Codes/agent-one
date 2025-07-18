f67eed710862f99fa428e6d61545f6f831d73f02
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-17 20:27:43 -0500

    more message spacing tweaks

--- src/components/a1/messages/parts-group.tsx
+++ src/components/a1/messages/parts-group.tsx
@@ -4,7 +4,7 @@ export const MessagePartsGroup = ({
   children: React.ReactNode;
 }) => {
   return (
-    <div className="bg-secondary text-secondary-foreground flex flex-col rounded-md max-w-3/4">
+    <div className="bg-secondary text-secondary-foreground flex flex-col rounded-md max-w-3/4 p-2">
       {children}
     </div>
   );
