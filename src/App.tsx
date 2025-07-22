c82bfc0c4acd4d8bc17e9c684783508d0e074f97
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-22 17:49:02 -0500

    add react router

--- src/App.tsx
+++ src/App.tsx
@@ -1,32 +1,14 @@
-import { NoMessagesGreeting } from "@/components/a1/empty-states/no-messages";
-import { MainChatInput } from "@/components/a1/input/main-chat-input";
-import { MessageParts } from "@/components/a1/messages";
-import { useChatMessages } from "@/contexts/use-chat/chat-hooks";
-import { cn } from "@/lib/utils";
+import { BrowserRouter, Route, Routes } from "react-router";
+
+import HomeRoute from "@/routes/home";
 
 function App() {
-  const { messages } = useChatMessages();
-
   return (
-    <main className="h-screen flex flex-col items-center justify-center">
-      <div className="w-full max-w-3xl h-full flex-1 flex flex-col">
-        <div className="flex-1 max-h-full relative overflow-auto space-y-0 p-0 pr-2 my-2">
-          {messages.length === 0 && <NoMessagesGreeting />}
-          {messages.map((message) => (
-            <div
-              key={message.id}
-              className={cn(
-                "flex",
-                message.role === "user" ? "justify-end" : "justify-start",
-              )}
-            >
-              <MessageParts message={message} />
-            </div>
-          ))}
-        </div>
-        <MainChatInput />
-      </div>
-    </main>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomeRoute />} />
+      </Routes>
+    </BrowserRouter>
   );
 }
 
---
+++ src/routes/home/index.tsx
@@ -0,0 +1,33 @@
+import { NoMessagesGreeting } from "@/components/a1/empty-states/no-messages";
+import { MainChatInput } from "@/components/a1/input/main-chat-input";
+import { MessageParts } from "@/components/a1/messages";
+import { useChatMessages } from "@/contexts/use-chat/chat-hooks";
+import { cn } from "@/lib/utils";
+
+function HomeRoute() {
+  const { messages } = useChatMessages();
+
+  return (
+    <main className="h-screen flex flex-col items-center justify-center">
+      <div className="w-full max-w-3xl h-full flex-1 flex flex-col">
+        <div className="flex-1 max-h-full relative overflow-auto space-y-0 p-0 pr-2 my-2">
+          {messages.length === 0 && <NoMessagesGreeting />}
+          {messages.map((message) => (
+            <div
+              key={message.id}
+              className={cn(
+                "flex",
+                message.role === "user" ? "justify-end" : "justify-start",
+              )}
+            >
+              <MessageParts message={message} />
+            </div>
+          ))}
+        </div>
+        <MainChatInput />
+      </div>
+    </main>
+  );
+}
+
+export default HomeRoute;
