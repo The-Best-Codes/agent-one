534c8dfabf8318cdfe6e00aa7e3a11563b50c418
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-23 15:44:42 -0500

    testing codemirror for performant markdown

--- src/components/a1/markdown/performant-markdown.tsx
+++ src/components/a1/markdown/performant-markdown.tsx
@@ -1,3 +1,53 @@
-export const PerformantMarkdown = ({ content }: { content: string }) => {
-  return <p>{content}</p>;
-};
+import { markdown, markdownLanguage } from "@codemirror/lang-markdown";
+import { EditorView } from "@codemirror/view";
+import CodeMirror from "@uiw/react-codemirror";
+import { memo } from "react";
+
+export const PerformantMarkdown = memo(({ content }: { content: string }) => {
+  const editorTheme = EditorView.theme({
+    "&": {
+      border: "none",
+      backgroundColor: "transparent !important",
+    },
+    "&.cm-focused": {
+      outline: "none",
+    },
+    ".cm-scroller": {
+      overflow: "auto",
+      fontFamily: "inherit",
+    },
+    ".cm-content": {
+      padding: "0px",
+    },
+  });
+
+  return (
+    <CodeMirror
+      autoFocus
+      defaultValue={content || "No content detected to display"}
+      minHeight="16px"
+      maxHeight="384px"
+      minWidth="100%"
+      className="bg-transparent text-sm"
+      extensions={[
+        markdown({ base: markdownLanguage }),
+        editorTheme,
+        EditorView.lineWrapping,
+      ]}
+      readOnly={true}
+      basicSetup={{
+        lineNumbers: false,
+        foldGutter: false,
+        highlightActiveLine: false,
+        highlightActiveLineGutter: false,
+        highlightSelectionMatches: false,
+        autocompletion: false,
+        searchKeymap: false,
+        lintKeymap: false,
+        completionKeymap: false,
+      }}
+    />
+  );
+});
+
+PerformantMarkdown.displayName = "PerformantMarkdown";
