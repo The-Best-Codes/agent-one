d74b06787e2180e0dd2540fc3a3764ed0044e811
The-Best-Codes <bestcodes.official@gmail.com>
2025-07-24 16:38:32 -0500

    update padding

--- src/components/a1/chat-message-loading.tsx
+++ src/components/a1/chat-message-loading.tsx
@@ -5,7 +5,7 @@ export const ChatMessageLoading = () => {
   const { status } = useChatStatus();
 
   return (
-    <div className="justify-end">
+    <div className="justify-end p-2">
       {status == "streaming" && (
         <Loader2Icon className="w-4 h-4 animate-spin text-gray-500" />
         // TODO: Replace above with typing animation or something like ChatGPT's indicator
