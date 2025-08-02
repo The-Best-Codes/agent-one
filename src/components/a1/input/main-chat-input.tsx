283fb5e8c806db7c4ec0716ea84be16cb7d1baa7
The-Best-Codes <bestcodes.official@gmail.com>
2025-08-02 18:14:57 -0500

    aria-hidden on cm placeholder

--- src/components/a1/input/main-chat-input.tsx
+++ src/components/a1/input/main-chat-input.tsx
@@ -27,6 +27,7 @@ const editorTheme = EditorView.theme({
   },
   "& .cm-placeholder": {
     color: "var(--muted-foreground);",
+    "aria-hidden": "true",
   },
   "&.cm-focused": {
     outline: "none",
