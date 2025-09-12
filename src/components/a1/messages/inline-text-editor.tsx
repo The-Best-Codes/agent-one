7db54d2ce407ef7477012462c7ccb075f1e0e2e6
The-Best-Codes <bestcodes.official@gmail.com>
2025-09-12 16:09:38 -0500

    Update inline-text-editor.tsx
    Signed-off-by: The-Best-Codes <bestcodes.official@gmail.com>

--- src/components/a1/messages/inline-text-editor.tsx
+++ src/components/a1/messages/inline-text-editor.tsx
@@ -72,7 +72,7 @@ const InlineTextEditorImpl = ({
         Prec.highest(
           keymap.of([
             {
-              key: "Enter",
+              key: settings.SUBMIT_KEY.value === "enter" ? "Enter" : "Ctrl-Enter",
               run: () => {
                 if (!disableEnter && onEnter) {
                   onEnter();
