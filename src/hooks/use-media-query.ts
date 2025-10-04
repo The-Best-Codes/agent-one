175f8800125c58ba40cccc927b740620a601f415
The-Best-Codes <bestcodes.official@gmail.com>
2025-10-04 17:19:42 -0500

    fix(lint): rm redundant `matches` check
    Signed-off-by: The-Best-Codes <bestcodes.official@gmail.com>

--- src/hooks/use-media-query.ts
+++ src/hooks/use-media-query.ts
@@ -5,15 +5,14 @@ export function useMediaQuery(query: string): boolean {
 
   useEffect(() => {
     const media = window.matchMedia(query);
-    if (media.matches !== matches) {
-      setMatches(media.matches);
-    }
 
     const listener = () => setMatches(media.matches);
+    listener();
+
     media.addEventListener("change", listener);
 
     return () => media.removeEventListener("change", listener);
-  }, [matches, query]);
+  }, [query]);
 
   return matches;
 }
