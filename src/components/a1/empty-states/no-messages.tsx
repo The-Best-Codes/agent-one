960d4876ec846a3e17010171dc90663c77111ede
The-Best-Codes <bestcodes.official@gmail.com>
2025-10-03 22:16:43 -0500

    fix unnessacary useeffect lint issue
    Signed-off-by: The-Best-Codes <bestcodes.official@gmail.com>

--- src/components/a1/empty-states/no-messages.tsx
+++ src/components/a1/empty-states/no-messages.tsx
@@ -1,28 +1,28 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
+
+const phrases = [
+  "What's on your mind?",
+  "Where should we begin?",
+  "How can I help you today?",
+  "I'm all ears!",
+  "What can I help with?",
+  "Where should we start?",
+  "Ask me anything.",
+  "Ready when you are.",
+  "What's on your mind today?",
+  "What are you working on?",
+  "What's on the agenda today?",
+  "How can I help?",
+  "What can I do for you?",
+];
+
+const getRandomPhrase = () => {
+  const randomIndex = Math.floor(Math.random() * phrases.length);
+  return phrases[randomIndex];
+};
 
 export const NoMessagesGreeting = () => {
-  const [currentPhrase, setCurrentPhrase] = useState("");
-
-  useEffect(() => {
-    const phrases = [
-      "What's on your mind?",
-      "Where should we begin?",
-      "How can I help you today?",
-      "I'm all ears!",
-      "What can I help with?",
-      "Where should we start?",
-      "Ask me anything.",
-      "Ready when you are.",
-      "What's on your mind today?",
-      "What are you working on?",
-      "What's on the agenda today?",
-      "How can I help?",
-      "What can I do for you?",
-    ];
-
-    const randomIndex = Math.floor(Math.random() * phrases.length);
-    setCurrentPhrase(phrases[randomIndex]);
-  }, []);
+  const [currentPhrase] = useState(getRandomPhrase);
 
   return (
     <div className="flex h-full items-center justify-center">
