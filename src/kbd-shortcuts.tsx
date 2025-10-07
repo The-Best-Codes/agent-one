f4d05635dcb9a9fa7f1b5285233041a4869bc183
The-Best-Codes <bestcodes.official@gmail.com>
2025-10-07 18:57:18 -0500

    Add keyboard shortcuts registry and initial shortcuts
    - Create src/lib/kbd-registry.ts with centralized shortcut definitions
    - Refactor src/kbd-shortcuts.tsx to use registry
    - Add CTRL+L shortcut to focus main chat input in main-chat-input.tsx
    - Add CTRL+, shortcut to open settings in kbd-shortcuts.tsx

--- src/kbd-shortcuts.tsx
+++ src/kbd-shortcuts.tsx
@@ -1,11 +1,16 @@
 import { useHotkeys } from "react-hotkeys-hook";
 import { useNavigate } from "react-router";
 
+import { kbdRegistry } from "@/lib/kbd-registry";
+
 export function KbdRegistry() {
   // TODO: Disable this in prod
   const navigate = useNavigate();
-  useHotkeys("ctrl+shift+q", () => {
+  useHotkeys(kbdRegistry.openTests, () => {
     navigate("/tests");
+  });
+  useHotkeys(kbdRegistry.openSettings, () => {
+    navigate("/settings");
   });
 
   return null;
---
+++ src/lib/kbd-registry.ts
@@ -0,0 +1,5 @@
+export const kbdRegistry = {
+  focusMainChatInput: "ctrl+l",
+  openSettings: "ctrl+,",
+  openTests: "ctrl+shift+q",
+};
