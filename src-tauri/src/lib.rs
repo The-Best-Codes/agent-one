16e8363e019ea7854d917988dce5284e140c0a91
The-Best-Codes <bestcodes.official@gmail.com>
2025-12-08 20:26:37 -0600

    feat: add os plugin
    Signed-off-by: The-Best-Codes <bestcodes.official@gmail.com>

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -47,6 +47,7 @@ pub fn run() {
             }
         }))
         .plugin(tauri_plugin_deep_link::init())
+        .plugin(tauri_plugin_os::init())
         .plugin(tauri_plugin_process::init())
         .plugin(tauri_plugin_updater::Builder::new().build())
         .plugin(tauri_plugin_notification::init())
