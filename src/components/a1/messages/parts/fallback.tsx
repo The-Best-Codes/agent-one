b200fcca1be1ffe54d68bdf0bc1dcbbcd4efa98a
The-Best-Codes <bestcodes.official@gmail.com>
2026-02-07 16:49:06 -0600

    fix invalid classname
    Signed-off-by: The-Best-Codes <bestcodes.official@gmail.com>

--- src/components/a1/messages/parts/fallback.tsx
+++ src/components/a1/messages/parts/fallback.tsx
@@ -1,7 +1,7 @@
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export const MessagePartFallback = ({ ...props }: any) => {
   return (
-    <div className="bg-destructive text-destructive-foreground flex max-w-full flex-col rounded-md">
+    <div className="bg-destructive flex max-w-full flex-col rounded-md text-white">
       <span>Unknown message part:</span>
       <pre className="font-mono">{JSON.stringify(props)}</pre>
     </div>
