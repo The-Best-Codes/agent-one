cc9222f6a7e474b5ebb6985dc96fd0ae38d75842
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-02-18 19:15:46 -0600

    add websocket plugin

--- src-tauri/src/lib.rs
+++ src-tauri/src/lib.rs
@@ -73,6 +73,7 @@ pub fn run() {
         .plugin(tauri_plugin_http::init())
         .plugin(tauri_plugin_notification::init())
         .plugin(tauri_plugin_clipboard_manager::init())
+        .plugin(tauri_plugin_websocket::init())
         .plugin(tauri_plugin_dialog::init())
         .plugin(tauri_plugin_fs::init())
         .plugin(tauri_plugin_shell::init())
