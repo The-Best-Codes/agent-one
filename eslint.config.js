06a1052df73e4f1f50aee7e946446f9adae5796f
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-02-21 20:14:46 -0600

    Update eslint.config.js
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -58,8 +58,6 @@ export default [
               "Loadables",
               "byok",
               "deeplink",
-              "dexie",
-              "Dexie",
             ],
           },
         },
