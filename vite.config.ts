757048c461483145a34f1855c4a0105c1f2d6b4a
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-03-06 15:02:18 -0600

    rm custom docker test stuff
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- vite.config.ts
+++ vite.config.ts
@@ -164,7 +164,7 @@ export default defineConfig(
             }
           : undefined,
         watch: {
-          ignored: ["**/src-tauri/**", "**/docker/**", "**/tests/**"],
+          ignored: ["**/src-tauri/**", "**/tests/**"],
         },
       },
 
