62816250c5dcfe3da82be2bcf202baabecaee258
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-03-06 15:37:29 -0600

    rm import sort from lint
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- eslint.config.js
+++ eslint.config.js
@@ -1,7 +1,6 @@
 import js from "@eslint/js";
 import reactHooks from "eslint-plugin-react-hooks";
 import reactRefresh from "eslint-plugin-react-refresh";
-import simpleImportSort from "eslint-plugin-simple-import-sort";
 import { globalIgnores } from "eslint/config";
 import globals from "globals";
 import tseslint from "typescript-eslint";
@@ -19,13 +18,6 @@ export default [
   reactRefresh.configs.vite,
   {
     files: ["**/*.{ts,tsx}"],
-    plugins: {
-      "simple-import-sort": simpleImportSort,
-    },
-    rules: {
-      "simple-import-sort/imports": "error",
-      "simple-import-sort/exports": "error",
-    },
     languageOptions: {
       ecmaVersion: 2020,
       globals: globals.browser,
