0d4637cee7b714092a66a9da6387a3bbcc5d1101
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-04-24 17:36:04 -0500

    use flatpak info for detection, clean up pkgbuild
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- src-tauri/src/utils/update_management.rs
+++ src-tauri/src/utils/update_management.rs
@@ -2,11 +2,13 @@ use std::{env, path::Path};
 
 const EXTERNAL_UPDATE_MARKER_PATH: &str = "/usr/lib/agent-one/updates-managed-externally";
 const EXTERNAL_UPDATE_ENV_VARS: &[&str] = &["SNAP", "SNAP_NAME", "FLATPAK_ID"];
+const FLATPAK_INFO_PATH: &str = "/.flatpak-info";
 
 #[tauri::command]
 pub fn is_update_managed_externally() -> bool {
     EXTERNAL_UPDATE_ENV_VARS
         .iter()
         .any(|name| env::var_os(name).is_some())
+        || Path::new(FLATPAK_INFO_PATH).exists()
         || Path::new(EXTERNAL_UPDATE_MARKER_PATH).exists()
 }
