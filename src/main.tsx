cb11ed64edb718e775ac96c7fc07599de0f0ab93
The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>
2026-09-13 16:03:11 -0500

    re-introduce delay duration for tooltips (700ms)
    Signed-off-by: The-Best-Codes <106822363+The-Best-Codes@users.noreply.github.com>

--- src/main.tsx
+++ src/main.tsx
@@ -28,7 +28,7 @@ ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
               <ModelProvider>
                 <UpdateProvider>
                   <Suspense fallback={<SuspenseFallback />}>
-                    <TooltipProvider>
+                    <TooltipProvider delayDuration={700}>
                       <App />
                     </TooltipProvider>
                   </Suspense>
